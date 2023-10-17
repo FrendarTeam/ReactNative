cb367ad3bd90e3c26ac5d912c3362a93c801a002
gwonyeong <qwe980121@naver.com>
2023-10-17 23:19:28 +0900

    Add: kakao login

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Alert,
   Image,
@@ -11,13 +11,28 @@ import {
 import kakaoLoginButton from 'assets/kakao_login_button.png';
 import {LoggedInParamList} from '../../AppInner';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {
+  login,
+  logout,
+  getProfile as getKakaoProfile,
+  shippingAddresses as getKakaoShippingAddresses,
+  unlink,
+} from '@react-native-seoul/kakao-login';
 
 type SignInScreenProps = NativeStackScreenProps<LoggedInParamList, 'Auth'>;
-
+// `https://kauth.kakao.com/oauth/authorize?client_id=bf0918fd28c5b739f8b7d48a524c7534&redirect_uri=http://localhost:4000/auth&response_type=code`;
 export default function Auth({navigation}: SignInScreenProps) {
+  const [result, setResult] = useState<string>('');
+
   const pressKakaoLoginButton = async (text: string) => {
-    Alert.alert(text, '로그인 구현 예정');
-    navigation.navigate('Main');
+    // 카카오페이지로 이동
+    try {
+      const token = await login();
+      console.log('token', token);
+      setResult(JSON.stringify(token));
+    } catch (err) {
+      console.error('login err', err);
+    }
   };
 
   return (
